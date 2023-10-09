1fdee6046ff4937b8bcf164cf0b61cabf0b53a37
MJCM08 <122966932+MJCM08@users.noreply.github.com>
2023-10-09 12:31:59 +0800

    second commit

---
+++ valid_and_invalid.php
@@ -0,0 +1,10 @@
+<?php
+// Valid constant names
+define("ONE",     "first thing");
+define("TWO2",    "second thing");
+define("THREE_3", "third thing");
+define("__THREE__", "third value");
+
+// Invalid constant names
+define("2TWO",    "second thing");
+?>
